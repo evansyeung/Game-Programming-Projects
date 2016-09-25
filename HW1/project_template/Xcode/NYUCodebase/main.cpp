24951b307d972f06adff5b276a235a11760cb364
Evans Yeung <eby210@nyu.edu>
2016-09-24 20:50:29 -0400

    Modified main.cpp for HW1

--- HW1/project_template/Xcode/NYUCodebase/main.cpp
+++ HW1/project_template/Xcode/NYUCodebase/main.cpp
@@ -4,12 +4,26 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
+#include "ShaderProgram.h"
 
 #ifdef _WINDOWS
 #define RESOURCE_FOLDER ""
 #else
 #define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
 #endif
+
+GLuint LoadTexture(const char *image_path) {
+    SDL_Surface *surface = IMG_Load(image_path);
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_BGRA,
+                 GL_UNSIGNED_BYTE, surface->pixels);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    SDL_FreeSurface(surface);
+    return textureID;
+}
 
 SDL_Window* displayWindow;
 
@@ -23,6 +37,29 @@ int main(int argc, char *argv[])
     glewInit();
 #endif
     
+    //Setup
+    ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+    
+    Matrix projectionMatrix;
+    Matrix modelMatrixShip;
+    Matrix viewMatrix;
+    Matrix modelMatrixUFOR;
+    Matrix modelMatrixUFOY;
+    
+    modelMatrixShip.Rotate(-45 * (3.1415926536 / 180));
+    
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    projectionMatrix.setOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
+    glUseProgram(program.programID);
+    
+    GLuint shipTexture = LoadTexture("playerShip1_Blue.png");
+    GLuint ufoRTexture = LoadTexture("ufoRed.png");
+    GLuint ufoYTexture = LoadTexture("ufoGreen.png");
+    
+    float lastFrameTicks = 0.0f;
+    
     SDL_Event event;
     bool done = false;
     while (!done) {
@@ -32,6 +69,71 @@ int main(int argc, char *argv[])
             }
         }
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        //Loop
+        glClearColor(1.0, 0.0, 0.5, 1.0);   //Changes background to pink
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        float ticks = (float)SDL_GetTicks()/1000.0f;
+        float elapsed = ticks - lastFrameTicks;
+        lastFrameTicks = ticks;
+        float angle = elapsed ;
+        
+        program.setModelMatrix(modelMatrixShip);
+        program.setProjectionMatrix(projectionMatrix);
+        program.setViewMatrix(viewMatrix);
+        
+    //shipTexture setup
+        glBindTexture(GL_TEXTURE_2D, shipTexture);
+        
+        modelMatrixShip.Translate(0.0, angle * 0.75, 0.0);
+        
+        float verticesShip[] = {-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, verticesShip);
+        glEnableVertexAttribArray(program.positionAttribute);
+        
+        float texCoords[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0};
+        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+        glEnableVertexAttribArray(program.texCoordAttribute);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDisableVertexAttribArray(program.positionAttribute);
+        glDisableVertexAttribArray(program.texCoordAttribute);
+
+        
+    //ufoRTexture setup
+        program.setModelMatrix(modelMatrixUFOR);
+        glBindTexture(GL_TEXTURE_2D, ufoRTexture);
+        
+        modelMatrixUFOR.Rotate(angle * 0.5);
+        
+        float verticesRUFO[] = {1.0, 1.0, 2.0, 1.0, 2.0, 2.0, 1.0, 1.0, 2.0, 2.0, 1.0, 2.0};
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, verticesRUFO);
+        glEnableVertexAttribArray(program.positionAttribute);
+        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+        glEnableVertexAttribArray(program.texCoordAttribute);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        glDisableVertexAttribArray(program.positionAttribute);
+        glDisableVertexAttribArray(program.texCoordAttribute);
+        
+    //ufoYTexture Setup
+        program.setModelMatrix(modelMatrixUFOY);
+        glBindTexture(GL_TEXTURE_2D, ufoYTexture);
+        
+        modelMatrixUFOY.Rotate(angle * 0.5);
+        
+        float verticesYUFO[] = {-2.0, -2.0, -1.0, -2.0, -1.0, -1.0, -2.0, -2.0, -1.0, -1.0, -2.0, -1.0};
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, verticesYUFO);
+        glEnableVertexAttribArray(program.positionAttribute);
+        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+        glEnableVertexAttribArray(program.texCoordAttribute);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        glDisableVertexAttribArray(program.positionAttribute);
+        glDisableVertexAttribArray(program.texCoordAttribute);
+        
         SDL_GL_SwapWindow(displayWindow);
     }
     
